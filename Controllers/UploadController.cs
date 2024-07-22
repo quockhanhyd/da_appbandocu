d191bee9864c9ab2363b50e4e232fe4f99b77671
Khanh <quockhanh01091@gmail.com>
2024-07-22 22:00:24 +0700

    commit

---
+++ Controllers/UploadController.cs
@@ -0,0 +1,31 @@
+﻿using DA_AppBanDoCu.ViewModels.Responses.Base;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DA_AppBanDoCu.Controllers
+{
+    /// <summary>
+    /// Quản lý người dùng
+    /// </summary>
+    [ApiController]
+    [Route("api/[controller]")]
+    //[AuthenPermission]
+    public class UploadController : Authentication
+    {
+        [HttpPost]
+        [Route("upload")]
+        public async Task<Result> UploadImage(IFormFile file)
+        {
+            if (file == null || file.Length == 0)
+                return Result.GetResultError("No file uploaded.");
+
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/images", file.FileName);
+
+            using (var stream = new FileStream(path, FileMode.Create))
+            {
+                await file.CopyToAsync(stream);
+            }
+
+            return Result.GetResultOk(path);
+        }
+    }
+}
