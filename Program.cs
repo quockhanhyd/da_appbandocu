84dc54d0847bb2f1c983d7414d39b3f1bad7e770
dangpd <phamducdang0403@gmail.com>
2024-07-24 00:45:37 +0700

    update code

--- Program.cs
+++ Program.cs
@@ -1,11 +1,25 @@
+﻿using DA_AppBanDoCu.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddTransient<ICategoryService, CategoryService>();
+// Thay đổi cấu hình để lắng nghe trên tất cả các IP
+builder.WebHost.UseUrls("http://0.0.0.0:5009", "https://0.0.0.0:7156");
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        builder =>
+        {
+            builder.AllowAnyOrigin()
+                   .AllowAnyMethod()
+                   .AllowAnyHeader();
+        });
+});
 
 var app = builder.Build();
 
@@ -17,7 +31,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseCors("AllowAll");
 app.UseAuthorization();
 
 app.MapControllers();
