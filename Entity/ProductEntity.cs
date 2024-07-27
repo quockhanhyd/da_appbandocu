a8b7b85d91ec637080a407037e25fa8d2d56bf16
dangpd <phamducdang0403@gmail.com>
2024-07-27 18:25:12 +0700

    update

--- Entity/ProductEntity.cs
+++ Entity/ProductEntity.cs
@@ -9,8 +9,8 @@ namespace DA_AppBanDoCu.Entity
         public int TotalAmount { get; set; } // số lượng trong kho
         public int TotalSold { get; set; } // số lượng đã bán
         public float Vote { get; set; } // đánh giá /5 sao
-        public double Price { get; set; } // giá bán
-        public double PriceSale { get; set; } // giá giảm
+        public int Price { get; set; } // giá bán
+        public int PriceSale { get; set; } // giá giảm
         public int PercentSale { get; set; } // phần trăm giảm giá
         public string? Description { get; set; } // mô tả
         public string ImageUrl { get; set; } // ảnh chính 'url1,url2'
