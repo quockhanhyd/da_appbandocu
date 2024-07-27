82a54de2a7c3e2627781d964352b4f68d6c0c467
dangpd <phamducdang0403@gmail.com>
2024-07-27 23:42:25 +0700

    update backedn

--- ViewModels/Requests/OrderGetListRequest.cs
+++ ViewModels/Requests/OrderGetListRequest.cs
@@ -5,4 +5,22 @@ namespace DA_AppBanDoCu.ViewModels.Requests
     public class OrderGetListRequest : BaseRequest
     {
     }
+
+    public class OrderParam
+    {
+        public int MerchanID { get; set; }
+    }
+
+    public class OrderStatus
+    {
+        public int Status { get; set; }
+    }
+
+    public class OrderStatusCount
+    {
+        public string StatusName { get; set; }
+
+        public int TotalCount { get; set; }
+        public int Status { get; set; }
+    }
 }
